b81d732caaa6cec591c73fcbb39917309f3e7e08
AndyChoi <AndyChoi4495@github.com>
2024-07-26 00:19:22 -0400

    update

--- src/model/data/aws/index.js
+++ src/model/data/aws/index.js
@@ -91,9 +91,6 @@ async function readFragmentData(ownerId, id) {
 // Get a list of fragment ids/objects for the given user from memory db. Returns a Promise
 async function listFragments(ownerId, expand = false) {
   const fragments = await metadata.query(ownerId);
-  /*   console.log('==============================================');
-  console.log(fragments);
-  console.log('=============================================='); */
 
   // If we don't get anything back, or are supposed to give expanded fragments, return
   if (expand || !fragments) {
