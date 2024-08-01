ad184f687315771b3e1ae6e6b27db052142a5755
AndyChoi <AndyChoi4495@github.com>
2024-08-01 17:32:55 -0400

    roll back fragment.js

--- src/model/fragment.js
+++ src/model/fragment.js
@@ -32,7 +32,7 @@ class Fragment {
     this.updated = updated || new Date().toISOString();
     this.type = type;
     this.size = size;
-    this.data = data.toString();
+    this.data = data ? data.toString() : null;
   }
 
   /**
